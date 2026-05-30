56c310ff22c31f8e38e40eadbd015e32ebe71c13
Dhiraj Kafle <dhirajkafle19@gmail.com>
2026-05-30 13:39:18 +0545

    Relationshipo Loading Styles/Techniques

---
+++ 11_relationship_loading_styles/joined_eager_loading/models.py
@@ -0,0 +1,53 @@
+from sqlalchemy import ForeignKey, Integer, String, Text, create_engine
+
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship
+)
+
+
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+
+class User(BaseModel):
+
+    __tablename__ = 'users'
+
+    name: Mapped[str] = mapped_column(String(50))
+    
+    posts: Mapped[list["Post"]] = relationship(
+        back_populates="user", 
+        cascade="all, delete-orphan",
+    )
+
+    def __repr__(self) -> str:
+        return f'<User {self.name}>'
+
+
+
+
+class Post(BaseModel):
+
+    __tablename__ = 'posts'
+
+    content: Mapped[str] = mapped_column(Text)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    
+    user: Mapped["User"] = relationship(back_populates="posts")
+
+    def __repr__(self) -> str:
+        return f'<Post {self.id}>'
---
+++ 11_relationship_loading_styles/selectin_loading/database.py
@@ -0,0 +1,44 @@
+from sqlalchemy import URL, create_engine
+
+from sqlalchemy.orm import (
+    sessionmaker,
+)
+
+
+from models import Base
+
+
+
+url_object = URL.create(
+    drivername="sqlite",
+    database="selectin_load.db"
+)
+
+
+
+
+'''
+    Why engine is created outside get_session() ??
+
+    - It is because these objects are heavy infrastructure objects that should be created only once for the entire application, not every time you request a session.
+
+    - SQLAlchemy expects one engine per database, so we create/define it only once. 
+    Then reuse it everywhere.
+
+'''
+
+
+engine = create_engine(url_object, echo=True)
+
+
+Base.metadata.create_all(engine)
+
+
+Session = sessionmaker(bind=engine)
+
+
+
+
+def get_session():
+
+    return Session()
