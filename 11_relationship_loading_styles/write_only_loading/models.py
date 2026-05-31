388965faeb39eb89ca9f294b7fff0754b8f907f0
Dhiraj Kafle <dhirajkafle19@gmail.com>
2026-05-31 11:07:59 +0545

    updated ...

---
+++ 11_relationship_loading_styles/write_only_loading/models.py
@@ -0,0 +1,54 @@
+from sqlalchemy import ForeignKey, String, Text
+
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship
+)
+
+
+
+class Base(DeclarativeBase):
+
+    pass
+
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+
+class User(BaseModel):
+
+    __tablename__ = 'users'
+
+    name: Mapped[str] = mapped_column(String(50))
+    
+    posts: Mapped[list["Post"]] = relationship(
+        back_populates="user", 
+        cascade="all, delete-orphan",
+        lazy="write_only"           ## data can only be add or deleted or updated.. ## write_only never loads related data into the memory.          
+    )
+
+    def __repr__(self) -> str:
+        return f'<User {self.name}>'
+
+
+
+class Post(BaseModel):
+
+    __tablename__ = 'posts'
+
+    content: Mapped[str] = mapped_column(Text)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    
+    user: Mapped["User"] = relationship(back_populates="posts")
+
+    def __repr__(self) -> str:
+        return f'<Post {self.id}>'
+
